eac6c9886c8795eddea37f606f9a55fb900d5e3f
rsingh612 <rsingh612@gmail.com>
2021-06-25 15:21:49 -0500

    Enabling module within config.php just incase its not picked up by setup:up

--- app/etc/config.php
+++ app/etc/config.php
@@ -226,6 +226,7 @@ return [
         'MSP_TwoFactorAuth' => 1,
         'Temando_Shipping' => 1,
         'Vertex_Tax' => 1,
-        'Yotpo_Yotpo' => 1
+        'Yotpo_Yotpo' => 1,
+        'Assessment_SimpleQueue' => 1
     ]
 ];
